2d660c0a146daa52921e701473bac0abaae15acc
itsashish <ashish.singh667@gmail.com>
2020-02-19 17:14:17 +0530

    FIle I/O operations

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
+var fs = require('fs');
 
-var events = require('events')
-var util = require('util');
+fs.writeFile("sample.txt",'My sample text Updated with Async Call',()=>{
 
-var Person = function(name){this.name = name;}
-util.inherits(Person,events.EventEmitter)
-
-var ashish = new Person('ashish');
-var mahesh = new Person('mahesh');
-var raju = new Person('raju');
-var people = [ashish,mahesh,raju];
-
-people.forEach((Person)=>{
-    Person.on('Speak',(mssg)=>{
-
-        console.log("The Person talking is: "+ Person.name +' ' + mssg);
-    });
-});
-// Event emitter Module Commit
-ashish.emit('Speak','How are you dude ?');
-
+    var readme = fs.readFileSync('sample.txt','utf8');
+    
+    console.log(readme);
+})
 
