edc458830616de03ee3b5d27261ab46ae5a3ba43
itsashish <ashish.singh667@gmail.com>
2020-03-14 19:04:39 +0530

    test

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-var http = require('http');
 
-var server = http.createServer((req,res)=>{
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// var http = require('http');
+
+// var server = http.createServer((req,res)=>{
     
-    console.log('request was made: ' + req.url)
+//     console.log('request was made: ' + req.url)
 
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("Hey Dude!")
-});
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end("Hey Dude!")
+// });
 
-server.listen(3000,'127.0.0.1');
-console.log('Port number: 3000 is UP!')
+// server.listen(3000,'127.0.0.1');
+// console.log('Port number: 3000 is UP!')
--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log(10 + 4)
+//Get two Users from database and sum two numbers
+
+const user1 = Users.readDBSync(1) //2 s
+console.log(user1)//1 ms
+const user2 = Users.readDBSync(2) //2 sec
+console.log(user2)//1 ms
+
+console.log(1+2) //1 sec
+//Total 5.2 sec
+
+
+
+const user3 = Users.readDBAsync(1, () => {
+    console.log(user1)
+}) // 0 - 2 sec
+
+const user4 = Users.readDBAsync(2, () =>{
+    console.log(user4)
+}) //0.1 ms - 2.1
+
+console.log(2+3) // .2 - 1 se c
+
+//Total 2.2 sec
+
+
+
